8e606b0d8a3ad4fbde38cb6288bfca8be052f8fb
Saketben10 <madmansaketkumar@gmail.com>
2025-08-16 10:12:54 +0530

    added orm and db

---
+++ src/app/protected/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      this should only be seen by the auth  users
+    </div>
+  );
+};
+
+export default Page;
--- src/modules/home/ui/components/home-sidebar/personal-section.tsx
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/sidebar";
 import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
 import Link from "next/link";
+import { useAuth, useClerk } from "@clerk/nextjs";
 
 const items = [
   {
@@ -33,6 +34,8 @@ const items = [
 ];
 
 export const PersonalSection = () => {
+  const { isSignedIn } = useAuth();
+  const clerk = useClerk();
   return (
     <SidebarGroup>
       <SidebarGroupLabel>You</SidebarGroupLabel>
@@ -45,7 +48,12 @@ export const PersonalSection = () => {
                   tooltip={item.title}
                   asChild
                   isActive={false} //TODO: change to look at current pathname
-                  onClick={() => {}} //TODO: Do something on click
+                  onClick={(e) => {
+                    if (!isSignedIn && item.auth) {
+                      e.preventDefault();
+                      return clerk.openSignIn();
+                    }
+                  }}
                 >
                   <Link href={item.url} className="flex items-center gap-4">
                     <item.icon />
